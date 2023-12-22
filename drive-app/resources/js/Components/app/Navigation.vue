3a0f5418fa7f097107bb872b5853d6c1760742f4
dylanxhernandez <dylan.x.h.developer@gmail.com>
2023-12-22 16:21:39 -0500

    feat: update navigation component with application logo markup

--- drive-app/resources/js/Components/app/Navigation.vue
+++ drive-app/resources/js/Components/app/Navigation.vue
@@ -1,10 +1,15 @@
 <script setup>
+    import ApplicationLogo from "@/Components/ApplicationLogo.vue"
 </script>
 
 <template>
-    <div>
-        Navigation Menu
-    </div>
+    <nav class="min-w-[200px]">
+        <div class="h-80px px-3 flex items-center gap-3">
+            <Link href="/">
+                <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+            </Link>
+        </div>
+    </nav>
 </template>
 
 <style scoped>
