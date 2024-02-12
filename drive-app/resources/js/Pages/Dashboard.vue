79be5955bd471d9bf2c47e16c97ab3c15e1161d1
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-02-12 15:48:30 -0500

    style: update padding in dasboard

--- drive-app/resources/js/Pages/Dashboard.vue
+++ drive-app/resources/js/Pages/Dashboard.vue
@@ -4,7 +4,7 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <AuthenticatedLayout>
+    <AuthenticatedLayout class="p-6">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
