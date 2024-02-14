2445d3f7f30d747d90fd1c82bf6796357564a036
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-02-14 17:55:38 -0500

    feat: add file model, add route service for files and folders, update components and routes

--- drive-app/app/Http/Controllers/FileController.php
+++ drive-app/app/Http/Controllers/FileController.php
@@ -11,4 +11,24 @@ class FileController extends Controller
     {
         return Inertia::render(component: 'MyFiles');
     }
+
+    public function createFolder(StoreFolderRequest $request)
+    {
+        $data = $request->validated();
+        $parent = $request->parent;
+
+        if (!$parent) {
+            $parent = $this->getRoot();
+        }
+
+        $file = new File();
+        $file->is_folder = 1;
+        $file->name = $data['name'];
+
+    }
+
+    private function getRoot()
+    {
+        return File::query()->whereIsRoot()->where('created_by', Auth::id())->firstOrFail();
+    }
 }
