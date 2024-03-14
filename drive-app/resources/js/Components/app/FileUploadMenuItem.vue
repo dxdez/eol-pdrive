9da563b88c73e80e5509c5f1a3c3299bc5193de5
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-03-14 14:00:57 -0400

    feat: install mitt and setup multi-upload input

--- drive-app/resources/js/Components/app/FileUploadMenuItem.vue
+++ drive-app/resources/js/Components/app/FileUploadMenuItem.vue
@@ -2,11 +2,16 @@
     <MenuItem v-slot="{ active }">
         <a href="#" class="text-gray-700 block px-4 py-2 text-sm">
             Upload Files
-            <input type="file" class="absolute left-0 top-0 bottom-0 right-0 cursor-pointer opacity-0">
+            <input @change="onChange" type="file" class="absolute left-0 top-0 bottom-0 right-0 cursor-pointer opacity-0" multiple>
         </a>
     </MenuItem>
 </template>
 <script setup>
-    import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+    import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
     import {ref} from "vue";
+    import {emitter, FILE_UPLOAD_STARTED} from "@/event-bus.js";
+
+    function onChange(ev) {
+        emitter.emit(FILE_UPLOAD_STARTED, ev.target.files);
+    }
 </script>
