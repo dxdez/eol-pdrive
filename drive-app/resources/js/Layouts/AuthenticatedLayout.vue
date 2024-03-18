40603e7126b06b73746332d96b3d58e43852af7e
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-03-18 14:27:42 -0400

    feat: add drop function for files/folders

--- drive-app/resources/js/Layouts/AuthenticatedLayout.vue
+++ drive-app/resources/js/Layouts/AuthenticatedLayout.vue
@@ -5,8 +5,30 @@
     import {onMounted, ref} from "vue";
     import {emitter, FILE_UPLOAD_STARTED} from "@/event-bus.js";
 
+    const dragOver = ref(false);
+
     function uploadFiles(files) {
         console.log(files);
+    }
+
+    function handleDrop(ev) {
+        dragOver.value = false;
+        const files = ev.dataTransfer.files
+
+        console.log(files);
+        if(!files.length) {
+            return;
+        }
+
+        uploadFiles(files);
+    }
+
+    function onDragOver() {
+        dragOver.value = true
+    }
+
+    function onDragLeave() {
+        dragOver.value = false
     }
 
     onMounted(() => {
@@ -17,14 +39,35 @@
 <template>
     <div class="h-screen bg-gray-50 flex w-full gap-4">
         <Navigation class="p-6 bg-zinc-200" />
-        <main class="flex flex-col flex-1 px-4 overflow-hidden p-6">
-            <div class="flex item-center justify-between w-full">
-                <SearchForm />
-                <UserSettingsDropdown />
-            </div>
-            <div class="flex-1 flex flex-col overflow-hidden">
-                <slot />
-            </div>
+        <main @drop.prevent="handleDrop"
+            @dragover.prevent="onDragOver"
+            @dragleave.prevent="onDragLeave"
+            class="flex flex-col flex-1 px-4 overflow-hidden p-6" :class="dragOver ? 'dropzone' : ''">
+
+            <template v-if="dragOver" class="text-gray-500 text-center py-8 text-sm">
+                DROP FILES HERE TO UPLOAD
+            </template>
+            <template v-else>
+                <div class="flex item-center justify-between w-full">
+                    <SearchForm />
+                    <UserSettingsDropdown />
+                </div>
+                <div class="flex-1 flex flex-col overflow-hidden">
+                    <slot />
+                </div>
+            </template>
         </main>
     </div>
 </template>
+
+<style scoped>
+    .dropzone {
+        width: 100%;
+        height: 100%;
+        color: #8d8d8d;
+        border: 2px dashed gray;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
