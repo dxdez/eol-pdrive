24dde032dfa44c40aa0c4560b935ab5d1002eefc
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-03-19 16:15:28 -0400

    feat: add file tree processing to file/folder upload

--- drive-app/app/Http/Requests/StoreFileRequest.php
+++ drive-app/app/Http/Requests/StoreFileRequest.php
@@ -9,6 +9,24 @@ use Illuminate\Validation\Rule;
 
 class StoreFileRequest extends ParentIdBaseRequest
 {
+    protected function prepareForValidation()
+    {
+        $paths = array_filter($this->relative_paths ?? [], fn($file) => $file != null);
+        $this->merge([
+            'file_paths' => $paths,
+            'folder_name' => $this->detectFolderName($paths)
+        ]);
+    }
+
+    protected function passedValidation()
+    {
+        $data = $this->validated();
+
+        $this->replace([
+            'file_tree' => $this->buildFileTree($this->file_paths, $data['files'])
+        ]);
+    }
+
     public function rules(): array
     {
         return array_merge(parent::rules(), [
@@ -28,7 +46,39 @@ class StoreFileRequest extends ParentIdBaseRequest
                         }
                     }
                 }
+            ],
+            'folder_name' => [
+                'nullable',
+                'string',
+                function ($attribute, $value, $fail) {
+                    if ($value) {
+                        $file = File::query()->where('name', $value)
+                            ->where('created_by', Auth::id())
+                            ->where('parent_id', $this->parent_id)
+                            ->whereNull('deleted_at')
+                            ->exists();
+
+                        if ($file) {
+                            $fail('Folder "' . $value . '" already exists.');
+                        }
+                    }
+                }
             ]
         ]);
+    }
+
+    public function detectFolderName($paths)
+    {
+        if(!$paths) {
+            return null;
+        }
+
+        $parts = explode("/", $paths[0]);
+        return $parts[0];
+    }
+
+    private function buildFileTree($filePaths, $files)
+    {
+        dd($filePaths, $files);
     }
 }
