2e7d6001d3851d1920a168ce74cbd2e381d4e824
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-04-16 13:23:26 -0400

    feat: add resources and handling for other image icons

--- drive-app/resources/js/Components/app/FileIcon.vue
+++ drive-app/resources/js/Components/app/FileIcon.vue
@@ -5,12 +5,20 @@
         </svg>
         <template v-else>
             <img class="max-w-full" v-if="isImage(file)" :src="`/images/icon/image.png`">
+            <img class="max-w-full" v-else-if="isPDF(file)" :src="`/images/icon/pdf.png`">
+            <img class="max-w-full" v-else-if="isAudio(file)" :src="`/images/icon/audio.png`">
+            <img class="max-w-full" v-else-if="isVideo(file)" :src="`/images/icon/video.png`">
+            <img class="max-w-full" v-else-if="isWord(file)" :src="`/images/icon/word.png`">
+            <img class="max-w-full" v-else-if="isExcel(file)" :src="`/images/icon/excel.png`">
+            <img class="max-w-full" v-else-if="isZip(file)" :src="`/images/icon/zip.png`">
+            <img class="max-w-full" v-else-if="isText(file)" :src="`/images/icon/txt-file.png`">
+            <img class="max-w-full" v-else :src="`/images/icon/attach-file.png`">
         </template>
     </span>
 </template>
 
 <script setup>
-    import {isImage} from "@/Helper/file-helper.js";
+    import {isAudio, isExcel, isImage, isPDF, isText, isVideo, isWord, isZip} from "@/Helper/file-helper.js";
 
     const {file} = defineProps({
         file: Object
