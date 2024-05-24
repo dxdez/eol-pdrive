8740590be0b3e2c61b9dc6d6710187008818249e
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-05-24 16:27:21 -0400

    feat: add download button component

---
+++ drive-app/resources/js/Components/app/DownloadFilesButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <PrimaryButton @click="download">
+        Download
+    </button>
+</template>
+
+<script setup>
+    import {useForm, usePage} from "@inertiajs/vue3";
+    import PrimaryButton from "@/Components/PrimaryButton.vue";
+
+    const page = usePage();
+    
+    const props = defineProps({
+        all: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        ids: {
+            type: Array,
+            required: false
+        }
+    })
+
+    function download() {
+        console.log("Download...");
+    }
+</script>
